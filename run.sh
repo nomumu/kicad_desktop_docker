b4158e92d589c8926319be7f31670ad10dd7d0d6
nomumu <nomumu-github@koso2-dan.ddo.jp>
2021-10-30 03:55:20 +0900

    Add functions to specify container names and ports

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-read -sp "sudo pass: " PASSWORD
+CONTAINER_NAME=${1:-kicad_desktop}
+NOVNC_PORT=${2:-15900}
 
-docker run --rm -itd \
+docker run --name $CONTAINER_NAME \
+	--rm -itd \
         -u $(id -u $USER):$(id -u $USER) \
         -w="/home/$USER" \
         --mount type=bind,src=/etc/passwd,dst=/etc/passwd,ro \
         --mount type=bind,src=/etc/group,dst=/etc/group,ro \
         --mount type=bind,src=/etc/shadow,dst=/etc/shadow,ro \
         --mount type=bind,src=`pwd`/homedir,dst=/home/$USER \
-        -p 15900:15900 \
+        -p $NOVNC_PORT:15900 \
         kicad_desktop_docker $PASSWORD
